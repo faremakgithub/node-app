552fb21b9d7c0db00ba0f0fe66b1c61c9133938b
Fare MAKOU <faremak02@gmail.com>
2022-03-02 21:47:40 +0100

    changed test.js and index.js text-1.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hey world ! Bonjour');
+ res.send('hey world ! Bonjour.');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3005);
