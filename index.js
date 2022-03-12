83416b627a96f1ff8c7936cd48e6c852c3b527bc
Fare MAKOU <faremak02@gmail.com>
2022-03-12 06:15:24 +0100

    changed test.js and index.js text-5 with port 3005.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hey world ! Bonjour Faremak le Boss');
+ res.send('hey world ! Bonjour Faremak');
 });
 //listen to port 3000 by default
-app.listen(process.env.PORT || 3006);
+app.listen(process.env.PORT || 3005);
 
 module.exports = app;
